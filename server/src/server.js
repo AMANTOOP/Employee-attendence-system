5005ab43e93d0d5b599cc5d7337f974ec92faf71
AMANTOOP <113962696+AMANTOOP@users.noreply.github.com>
2025-11-30 11:03:39 +0530

    Initial setup done

---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+// src/server.js
+const express = require("express");
+const connectDB = require("./config/db");
+const app = require("./app");
+require("dotenv").config();
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
